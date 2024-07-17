79dbd322d9c97a7b607cfd7d52bee07131121300
SurajJankar <145422239+SurajJankar@users.noreply.github.com>
2024-07-17 18:00:23 +0530

    new change is here

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
 import './App.css'
-import NavBar from './components/NavBar'
-import Footer from './components/Footer';
+
 
 function App() {
   return (
-    <>
-    <NavBar />
-    <Footer />
-    </>
+    <h1>Hiiii</h1>
   );
 }
 
